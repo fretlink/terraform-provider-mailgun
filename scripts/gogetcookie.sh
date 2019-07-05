065bf5d75bfd81d439ea42434463f796128d08f1
Alexandre Garand <alexandre.garand@fretlink.com>
2019-07-05 14:02:37 +0200

    add shellcheck to the makeffile and .travis.yml and fix the scripts to have no error with it

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
- touch ~/.gitcookies
- chmod 0600 ~/.gitcookies
+touch ~/.gitcookies
+chmod 0600 ~/.gitcookies
 
- git config --global http.cookiefile ~/.gitcookies
+git config --global http.cookiefile ~/.gitcookies
 
- tr , \\t <<\__END__ >>~/.gitcookies
+tr , \\t <<\__END__ >>~/.gitcookies
 .googlesource.com,TRUE,/,TRUE,2147483647,o,git-alexandre.namebla.gmail.com=1/OtfvUDYg3VAHfIxaqjAuv8MJqu6--gSU_zSkD8YkKPc
+__END__
