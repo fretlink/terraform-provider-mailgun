7cbb92efb5e84de877976830ee04798c1cc808b8
Alexandre Garand <alexandre.garand@fretlink.com>
2019-07-08 15:46:38 +0200

    add newline and commentary for clarity

--- GNUmakefile
+++ GNUmakefile
@@ -48,6 +48,7 @@ test-compile:
 
 website-githubpages:
 	bash scripts/launch-website.sh
+
 website:
 ifeq (,$(wildcard $(GOPATH)/src/$(WEBSITE_REPO)))
 	echo "$(WEBSITE_REPO) not found in your GOPATH (necessary for layouts and assets), get-ting..."
--- scripts/launch-website.sh
+++ scripts/launch-website.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#the sleep and the subquery & are here to ensure that wget is called after make website have done its work so that the website is gotten correctly
 (sleep 10;
 wget --recursive --no-parent --domains localhost --page-requisites --html-extension --convert-links --no-clobber http://localhost:4567/docs/providers/mailgun/;
 
